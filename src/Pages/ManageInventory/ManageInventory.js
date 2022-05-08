186edd3e06935434b7d9b4a9b334a6b54624bc33
ariful892 <ariful00892@gmail.com>
2022-05-08 18:30:18 +0600

    navigate to add item page from manage inventory

--- src/Pages/ManageInventory/ManageInventory.js
+++ src/Pages/ManageInventory/ManageInventory.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import useItems from '../../hooks/useItems';
 import ManageInventoryItem from '../MnanageInventoryItem/ManageInventoryItem';
+
+import './ManageInventory.css';
 
 const ManageInventory = () => {
 
     const [items, setItems] = useItems();
+    const navigate = useNavigate();
 
     const handleDelete = id => {
         const proceed = window.confirm('Are you sure?');
@@ -23,9 +27,16 @@ const ManageInventory = () => {
         }
     }
 
+    const navigateToAddItem = () => {
+        navigate('/add');
+    }
+
     return (
         <div className='m-5'>
-            <h2 className='text-white text-center'>Manage Inventory {items.length}</h2>
+            <h2 className='add-item-title'>Manage Inventory: {items.length}</h2>
+            <div className='text-end'>
+                <button onClick={navigateToAddItem} className='new-item-btn px-3'>Add New Item</button>
+            </div>
             {
                 items.map(item => <ManageInventoryItem
                     key={item._id}
